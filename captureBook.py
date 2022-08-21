d84f3a2a4e6cd53845efb6216cc4ead35a539fbb
felipe-l <felipelopez@email.arizona.edu>
2022-08-21 16:50:08 -0700

    Uncommended Lines

--- captureBook.py
+++ captureBook.py
@@ -4,14 +4,14 @@ import downloadBook
 
 BOOK_NAME = "myBook"
 startingPage = 1
-#
-# downloadBook.takeScreenshot(BOOK_NAME, startingPage)
-# print("BOOK DOWNLOADED")
-#
-# mergeImages = cutImageToPDF.gatherImageNames(BOOK_NAME)
-# print(mergeImages)
-# input("Start Cropping above images")
-# cutImageToPDF.cutImages(mergeImages, BOOK_NAME)
+
+downloadBook.takeScreenshot(BOOK_NAME, startingPage)
+print("BOOK DOWNLOADED")
+
+mergeImages = cutImageToPDF.gatherImageNames(BOOK_NAME)
+print(mergeImages)
+input("Start Cropping above images")
+cutImageToPDF.cutImages(mergeImages, BOOK_NAME)
 
 imageNames = createPDF.sortImageNames(BOOK_NAME)
 input("Press Enter to create PDF")
